5ed87a8a99ecc41ef0697864724be88c23ee1feb
mayuu663 <128414371+mayuu663@users.noreply.github.com>
2025-04-02 14:09:09 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
-// pages/index.js
+import Head from "next/head";
+import Script from "next/script";
+
 export default function Home() {
-  return <h1>IDOL Shooter is working! 🎮</h1>;
+  return (
+    <>
+      <Head>
+        <title>IDOL Shooter</title>
+      </Head>
+
+      <canvas id="gameCanvas" width="480" height="640"></canvas>
+
+      <Script src="/game.js" strategy="afterInteractive" />
+    </>
+  );
 }
