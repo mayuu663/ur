ada59e21034edb39bae15d0d767952176555b97a
mayuu663 <128414371+mayuu663@users.noreply.github.com>
2025-04-02 12:29:08 +0900

    Update game.js

--- public/game.js
+++ public/game.js
@@ -377,3 +377,10 @@ async function endGame() {
 }
 
 // 既存の game.js に追加する形でOK！
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  return parts.length === 2 ? parts.pop().split(';').shift() : null;
+}
+
+const fid = getCookie("fid") || 0;
