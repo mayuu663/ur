ba03c2d7482a0bb116ffdeb4b679afb7f3e8c77a
mayuu663 <128414371+mayuu663@users.noreply.github.com>
2025-04-02 16:31:36 +0900

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "100px", color: "hotpink" }}>
-      <h1>IDOL Shooter is back! 💖</h1>
-      <p>ページが表示されてるならOK！</p>
-    </div>
-  );
-}
